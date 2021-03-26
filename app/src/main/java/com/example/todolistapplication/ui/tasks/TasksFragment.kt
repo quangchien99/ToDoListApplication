ced32d961993ac892c0e22965adf448ed00230a4
Quangchien99 <phamquangchien170499@gmail.com>
2021-03-26 09:17:29 +0700

    set up navigation

---
+++ app/src/main/java/com/example/todolistapplication/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.example.todolistapplication.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.todolistapplication.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
